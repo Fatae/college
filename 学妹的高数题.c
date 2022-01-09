f168c73fb77c159a095e7181a8eae9ce20557e99
pc s <3051262892@qq.com>
2022-01-09 16:01:14 +0800

    周赛

---
+++ 学妹的高数题.c
@@ -0,0 +1,16 @@
+//#define  _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//	int p;
+//	scanf("%d", &p);
+//	if (p > 1)
+//		printf("infinity");
+//	else if (p == 1)
+//		printf("1");
+//	else
+//		printf("0");
+//
+//	return 0;
+//}
